70feb42a78e347e0d2b8fb2f2f620ff46ee696f2
globe-trotter <rishwan25s@gmail.com>
2020-08-08 02:44:30 +0530

    deleted unwanted imports

--- openLink.py
+++ openLink.py
@@ -2,9 +2,6 @@ import webbrowser
 import time
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 opts=Options()
 opts.add_experimental_option('debuggerAddress', 'localhost:9222')
 driver= webdriver.Chrome(chrome_options=opts)
